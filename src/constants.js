0e05425bbdbe50e35e50707f1ce4dfff0ebce987
EmersonFeng <99048255+Emerson-Feng@users.noreply.github.com>
2024-04-15 18:23:24 -0400

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/Emerson-Feng" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube or Bilibili. 
+  I also enjoy networking. Go connect with me on <a href="https://www.linkedin.com/in/yucongfeng/" target="_blank">my LinkedIn profile</a>!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@MrBeast" target="_blank">MrBeast</a>.<a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/Emerson-Feng/MyResume/blob/main/Yucong_Feng_2024.pdf" target="_blank">Check it out?</a>
+  Contact me at feng.yuco@northeastern.edu or emersonfeng123@gmail.com if you have any interesting job opportunities!`,
+  projects: `Check out my project in <a href="https://github.com/Emerson-Feng" target="_blank">here</a>.`,
+  library: `There are a lot of programming books on my shelves. There are also many history books.
+  I probably only read part of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Yucong Feng's portfolio, just close the tab. Have a nice day! :)`,
+};
